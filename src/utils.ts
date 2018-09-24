ba652cf5b3b2df2deaae4795a5e1e88d803f0089
bschwinn <bschwinn@gmail.com>
2018-09-24 17:26:02 -0400

    fix bug in bytes display function

--- src/utils.ts
+++ src/utils.ts
@@ -4,11 +4,11 @@ const GB = MB * 1000;
 
 export const formatBytes = (size:number, places: number) => {
     if (size > GB) {
-        return (size - GB).toFixed(places) + 'GB'
+        return (size / GB).toFixed(places) + 'GB'
     } else if (size > MB) {
-        return (size - MB).toFixed(places) + 'MB'
+        return (size / MB).toFixed(places) + 'MB'
     } else if (size > KB) {
-        return (size - KB).toFixed(places) + 'KB'
+        return (size / KB).toFixed(places) + 'KB'
     }
     return size.toLocaleString();
 }
